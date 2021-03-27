67eeca1eb8d59f42ce07c4fa32059e90a4ebed4f
MintyFresh122 <81022579+MintyFresh122@users.noreply.github.com>
2021-03-27 15:50:12 +0000

    Create reversortTest.py

---
+++ reversortTest.py
@@ -0,0 +1,11 @@
+def reversort(L):
+    cost = 0
+    for i in range(0, len(L)-1):
+        minimum = min(L[i:])
+        j = L.index(minimum)
+        if i == 0:
+            L = L[:i] + L[j::-1] + L[j+1:]
+        else:
+            L = L[:i] + L[j:i-1:-1] + L[j+1:]
+        cost += j-i+1
+    return cost
